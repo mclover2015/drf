e63b5931450e62c90b416fd1d7ef2b02a8ead743
Kirill Khokhlov <flizarrq@example.com>
2026-04-18 14:21:34 +0300

    fixed gitignore file

--- cars/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class CarModel(models.Model):
-    class Meta:
-        db_table = 'cars'
-    brand = models.CharField(max_length=20)
-    price = models.IntegerField()
-    year = models.IntegerField()
-
